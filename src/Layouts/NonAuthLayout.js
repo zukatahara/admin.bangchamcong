c806900ade31aec7824c3d51db686f5a421c10eb
Thuandeptrai <tvc4567@gmail.com>
2023-03-14 18:50:00 +0700

    Update

--- src/Layouts/NonAuthLayout.js
+++ src/Layouts/NonAuthLayout.js
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 
 //redux
 import { useSelector } from "react-redux";
+import { useProfile } from '../Components/Hooks/UserHooks';
+import { setAuthorization } from '../helpers/api_helper';
+import { logoutUser } from '../store/actions';
 
 const NonAuthLayout = ({ children }) => {
+    const history = useHistory();
+
     const {
         layoutModeType,
     } = useSelector(state => ({
         layoutModeType: state.Layout.layoutModeType,
     }));
-
+    const { userProfile, loading, token } = useProfile();
+    useEffect(() =>{
+        const userToken = sessionStorage.getItem("authUser")
+        const Token = JSON.parse(sessionStorage.getItem("authUser"))
+        if (userToken) {
+            setAuthorization(Token);
+            history.push("/user/list")
+          }
+    })
     useEffect(() => {
         if (layoutModeType === "dark") {
             document.body.setAttribute("data-layout-mode", "dark");
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -494,7 +494,9 @@ export const updateStatusOfRead = () => {
 };
 //
 export const getListBBCUser = () => {
-  return axios.get(`${url.API_USER}/getAllUser`);
+  return axios.get(`${url.API_USER}/getAllUser`,{ headers:{
+    'Access-Control-Allow-Credentials':true
+  }});
 };
 export const createNewBCCUser = (data) => {
   return axios.post(`${url.API_USER}/register`, data);
