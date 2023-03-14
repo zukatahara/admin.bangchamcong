0ae53a9ce774e247cdb5c792031157fd9b74c0ee
Thuandeptrai <tvc4567@gmail.com>
2023-03-14 17:58:44 +0700

    asdasd

--- src/Routes/AuthProtected.js
+++ src/Routes/AuthProtected.js
@@ -6,17 +6,32 @@ import { useDispatch } from "react-redux";
 import { useProfile } from "../Components/Hooks/UserHooks";
 
 import { logoutUser } from "../store/actions";
+import axios from "axios";
 
-const AuthProtected = (props) => {
+const AuthProtected =  (props) =>  {
   const dispatch = useDispatch();
   const { userProfile, loading, token } = useProfile();
-  useEffect(() => {
+  useEffect(  () => {
+    const getUser = async () =>
+    {
+      const userList = await axios.get(process.env.REACT_APP_API_URL + "/api/v1/users/init",{headers:{
+        Authorization:"Bearer eyJhbGciOiJIUzI1NiasdIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjQwZmQ3NWU4YTNkODg4NTIyNmY0OGIwIiwiaWF0IjoxNjc4NzYwMTY2LCJleHAiOjE2Nzg4NDY1NjZ9.Z0_vlnWiAFx1rPHeJx09cvkCj1Np43et-qqtLgXRj2g"
+      }}).then((res) =>{
+
+      }).catch(() =>
+      {
+      dispatch(logoutUser());
+      
+      })
+
+
+    }
     if (userProfile && !loading && token) {
       setAuthorization(token);
     } else if (!userProfile && loading && !token) {
       dispatch(logoutUser());
     }
-    
+   getUser()
   }, [token, userProfile, loading, dispatch]);
 
   /*
